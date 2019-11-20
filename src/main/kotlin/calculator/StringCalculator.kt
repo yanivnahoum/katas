8349cb0f0690816623e41468b7520354043fd4fc
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2019-11-20 09:19:17 +0200

    [StringCalculator] Added Kotlin implementation and test, added Kotlin deps to pom

---
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -0,0 +1,18 @@
+package com.att.training.katas.kt.calculator
+
+class StringCalculator {
+    fun add(input: String): Int {
+        if (input.isEmpty()) return 0
+
+        val tokens = input.split(',', '\n')
+        validate(tokens)
+        return tokens.map { it.toInt() }.sum()
+    }
+
+    private fun validate(tokens: List<String>) {
+        val negatives = tokens.filter { it.startsWith("-") }
+        if (negatives.isNotEmpty()) {
+            throw IllegalArgumentException("Negatives not allowed: $negatives")
+        }
+    }
+}
